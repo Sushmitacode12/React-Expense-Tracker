228ec82e6b0d85124664d684033f22b5baab4c69
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-03-20 06:01:00 +0530

    Logout Added

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,11 +1,19 @@
 import React, { useContext } from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import AuthContext from '../Store/auth-context';
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  // const authCtx = useContext(AuthContext);
+  // const isLoggedIn = authCtx.isLoggedIn
+  const history = useHistory();
   const authCtx = useContext(AuthContext);
-  const isLoggedIn = authCtx.isLoggedIn
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    history.replace("/");
+  };
 
   return (
     <div className={classes.head}>
@@ -15,6 +23,7 @@ const MainNavigation = () => {
             {isLoggedIn && <h3>Products</h3>}
             {isLoggedIn && <NavLink to="/profile">Profile</NavLink>} 
             {isLoggedIn && <h3>About Us</h3>} 
+            {isLoggedIn && <button onClick={logoutHandler}>Logout</button>}
         </header>
     </div>
   )
