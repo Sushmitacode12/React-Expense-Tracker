5a0f1676fdb5b98c851d86dcd4164d0654142daf
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-03-26 01:42:00 +0530

    POST and GET API Added

--- src/components/features/Form.js
+++ src/components/features/Form.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ExpenseContext } from "../store/ExpenseContext";
 import classes from "./Form.module.css";
 
@@ -18,12 +18,71 @@ export const Form = () => {
   const categoryHandler = (e) => {
     setCategory(e.target.value);
   };
+  const getApiHandler = () => {
+    fetch(
+      `https://crudcrud.com/api/3f386f3ba9b2444fb77bd739335eee9f/expenses`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication Failed";
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        expenseCtx.setExpenses(data);
+      })
+      .catch((err) => alert(err.message));
+  };
+  useEffect(() => {
+    getApiHandler()
+  }, []);
+
   const submitHandler = (e) => {
     e.preventDefault();
-    expenseCtx.setExpenses([
-      ...expenseCtx.expenses,
-      { expense, description, category },
-    ]);
+    // expenseCtx.setExpenses([
+    //   ...expenseCtx.expenses,
+    //   { expense, description, category },
+    // ]);
+    fetch(
+        `https://crudcrud.com/api/3f386f3ba9b2444fb77bd739335eee9f/expenses`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            expense,
+            description,
+            category,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            return res.json().then((data) => {
+              let errorMessage = "Authentication Failed";
+              throw new Error(errorMessage);
+            });
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          getApiHandler()
+        })
+        .catch((err) => alert(err.message));
   };
   return (
     <form className={classes.form} onSubmit={submitHandler}>
